85f548a39238a3faf2714f99351a9444a27a5dd3
ksenia <ksenia.timofeeva@gmail.com>
2017-11-29 00:53:15 +0300

    change board styles

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,18 +1,15 @@
 function generalInfoListener(message) {
-    console.log(message);
     updateGeneralInfo();
 }
 
 function driverInfoListener(message) {
-    console.log(message);
     updateDriverInfo();
 }
 
 function truckInfoListener(message) {
-    console.log(message);
     updateTruckInfo();
 }
 
 function orderInfoListener(message) {
-    console.log(message)
+    updateOrderInfo();
 }
